40651b20cbfa3d3b857aa26d2390c5864c97529c
Piyush Srivastava <piyush.0101@gmail.com>
2015-01-06 09:53:41 -0600

    Adding a module to install go server

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,2 +1,6 @@
 include ntp
 include java
+
+class { 'go-server':
+  version => '14.4.0-1356'
+}
---
+++ modules/go-server/manifests/init.pp
@@ -0,0 +1,30 @@
+class go-server(
+  $version = undef
+) {
+  file { '/opt/go-server':
+    ensure => directory,
+  }
+
+  exec { 'go-package':
+    command => "/usr/bin/wget http://dl.bintray.com/gocd/gocd-deb/go-server-$version.deb -O /opt/go-server/go-server.deb",
+    cwd => '/opt/go-server',
+    creates => '/opt/go-server/go-server.deb',
+  }
+
+  package { 'unzip':
+    ensure => installed,
+  }
+
+  package { 'go-server':
+    provider => dpkg,
+    ensure => installed,
+    source => '/opt/go-server/go-server.deb',
+    require => Exec['go-package'],
+  }
+
+  service { 'go-server':
+    ensure => running,
+    enable => true,
+    require => Package['go-server'],
+  }
+}
