da39f9854b7631039e37acdbff4ffd26a573a512
Whitaker, Greg <Greg.Whitaker@nike.com>
2015-05-19 08:19:31 -0700

    Add jdk

--- modules/java/manifests/debian.pp
+++ modules/java/manifests/debian.pp
@@ -3,8 +3,8 @@ class java::debian {
   exec { 'apt update':
     command => '/usr/bin/apt-get update',
   } ->
-
-  package { 'openjdk-7-jre':
-      ensure => installed
+  
+  package { 'openjdk-7-jdk':
+    ensure => installed
   }
 }
